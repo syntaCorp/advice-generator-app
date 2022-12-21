547b17bc72ed55e0cc6ea3a5c73e60bea56d4ec1
cyberstrong <108492938+syntaCorp@users.noreply.github.com>
2022-12-21 21:33:58 +0000

    full functionality/responsiveness

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 // https://api.adviceslip.com/advice
 
+//select elements
+const quoteBtn = document.querySelector('.dice');
+const advice = document.querySelector('#advice-id');
+const quote = document.querySelector('.advice-quote');
+
+
+//fetch quote and insert into page
+const render = () => {
+  fetch('https://api.adviceslip.com/advice')
+    .then(response => response.json())
+    .then((data) => {
+      advice.innerText = `ADVICE #${data['slip'].id}`;
+      quote.innerText = `${data['slip'].advice}`;  
+    })
+}
+
+//update advice on click
+quoteBtn.addEventListener('click', render);
